4ff124094e2986b6c2dd3d631fd3d8fbe56f1c0e
Martin Ohl <Martin.Ohl@ohl-net.eu>
2021-09-06 17:06:24 +0200

    v.0.2
    Added response actions option.

--- mar_workspace_api.py
+++ mar_workspace_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# written by mohlcyber 17/03/2021 v0.1
+# written by mohlcyber 06.09.2021 v0.2
 # This script to pull Threat Detection from the Active Response Workspace
 
 import logging
@@ -22,6 +22,7 @@ class EPO():
         self.user = args.epo_user
         self.pw = args.epo_pw
         self.hours = args.hours
+        self.action = args.action
 
         self.session = requests.Session()
 
@@ -43,8 +44,8 @@ class EPO():
 
     def auth(self):
         data = {
-            "j_username": self.user,
-            "j_password": self.pw
+            'j_username': self.user,
+            'j_password': self.pw
         }
 
         res = self.session.post('https://{0}:{1}/core/j_security_check'.format(self.epo, self.port),
@@ -57,7 +58,7 @@ class EPO():
             self.logger.error('Error in auth(). {0}-{1}'.format(str(res.status_code), res.text))
             sys.exit()
 
-    def get_events(self, token):
+    def get_events(self):
         params = [
             {
                 'field': 'processAccumSeverity',
@@ -77,7 +78,7 @@ class EPO():
 
         res = self.session.get('https://{0}:{1}/rest/edr/v1/traces/list?filter={2}&limit=100&since={3}'
                                '&orion.user.security.token={4}'
-                               .format(self.epo, self.port, json.dumps(params), self.hours, token))
+                               .format(self.epo, self.port, json.dumps(params), self.hours, self.token))
 
         if res.ok:
             return res.text
@@ -85,7 +86,7 @@ class EPO():
             self.logger.error('Error in get_ws_events(). {0}-{1}'.format(str(res.status_code), res.text))
             sys.exit()
 
-    def get_rep(self, md5, sha1, sha256, token):
+    def get_rep(self, md5, sha1, sha256):
         params = {
             'hashes': [[
                 {'value': md5, 'type': 'md5'},
@@ -95,7 +96,7 @@ class EPO():
         }
 
         res = self.session.get('https://{0}:{1}/rest/edr/v1/pe-reputations?query={2}&orion.user.security.token={3}'
-                               .format(self.epo, self.port, json.dumps(params), token))
+                               .format(self.epo, self.port, json.dumps(params), self.token))
 
         if res.ok:
             return res.text
@@ -103,10 +104,10 @@ class EPO():
             self.logger.error('Error in get_rep(). {0}-{1}'.format(str(res.status_code), res.text))
             sys.exit()
 
-    def get_hosts(self, sha256, token):
+    def get_hosts(self, sha256):
         res = self.session.get('https://{0}:{1}/rest/edr/v1/traces/SHA256-{2}/hosts?limit=1000&sort=host'
                                '&sortDirection=asc&since={3}&orion.user.security.token={4}'
-                               .format(self.epo, self.port, sha256, self.hours, token))
+                               .format(self.epo, self.port, sha256, self.hours, self.token))
 
         if res.ok:
             return res.text
@@ -114,10 +115,10 @@ class EPO():
             self.logger.error('Error in get_rep(). {0}-{1}'.format(str(res.status_code), res.text))
             sys.exit()
 
-    def get_traces(self, md5, sha1, sha256, maguid, token):
+    def get_traces(self, md5, sha1, sha256, maguid):
         res = self.session.get('https://{0}:{1}/rest/edr/v1/traces/events?md5={2}&sha1={3}&sha256={4}&maGuid={5}'
                                '&timeframe={6}&limit=1000&orion.user.security.token={7}'
-                               .format(self.epo, self.port, md5, sha1, sha256, maguid, self.hours, token))
+                               .format(self.epo, self.port, md5, sha1, sha256, maguid, self.hours, self.token))
 
         if res.ok:
             return res.text
@@ -125,33 +126,78 @@ class EPO():
             self.logger.error('Error in get_rep(). {0}-{1}'.format(str(res.status_code), res.text))
             sys.exit()
 
+    def take_action(self, action, md5, sha1, sha256, fname):
+        payload = {
+            'md5': md5,
+            'sha1': sha1,
+            'sha256': sha256,
+            'interpreterMD5': None,
+            'interpreterName': None,
+            'interpreterSha1': None,
+            'interpreterSha256': None,
+            'arguments': {
+                'hostData': [],
+                'fileName': fname
+            }
+        }
+
+        if action.upper() == 'DISMISS':
+            payload['command'] = 'DISMISS_THREAT'
+        elif action.upper() == 'MALICIOUS':
+            payload['command'] = 'CHANGE_REPUTATION'
+            payload['arguments']['trustLevel'] = 'KNOWN_MALICIOUS'
+        elif action.upper() == 'TRUSTED':
+            payload['command'] = 'CHANGE_REPUTATION'
+            payload['arguments']['trustLevel'] = 'KNOWN_GOOD'
+
+        res = self.session.post('https://{0}:{1}/rest/edr/v1/actions?orion.user.security.token={2}'
+                                .format(self.epo, self.port, self.token),
+                                json=payload)
+
+        if res.ok:
+            self.logger.info('Threat {0} got marked as {1}.'.format(fname, action))
+        else:
+            self.logger.error('Error in take_action(). {0}-{1}'.format(str(res.status_code), res.text))
+            sys.exit()
 
     def main(self):
         login = self.auth()
         page = BS(login, features="html.parser")
-        token = page.find('input', {'id': 'orion.user.security.token'}).get('value')
+        self.token = page.find('input', {'id': 'orion.user.security.token'}).get('value')
 
-        events = self.get_events(token)
+        events = self.get_events()
         for event in json.loads(events)['threats']:
             md5 = event['md5Hash']
             sha1 = event['sha1Hash']
             sha256 = event['sha256Hash']
 
-            reputations = self.get_rep(md5, sha1, sha256, token)
+            reputations = self.get_rep(md5, sha1, sha256)
             event['reputations'] = json.loads(reputations)
 
             systems = []
-            hosts = self.get_hosts(sha256, token)
+            hosts = self.get_hosts(sha256)
             for host in json.loads(hosts)['items']:
                 maguid = host['guid']
-                traces = self.get_traces(md5, sha1, sha256, maguid, token)
+                traces = self.get_traces(md5, sha1, sha256, maguid)
                 host['traces'] = json.loads(traces)
                 systems.append(host)
 
             event['hosts'] = systems
 
             self.logger.info(json.dumps(event))
-            sys.exit()
+
+            # Add reaction option
+            if self.action.upper() == 'YES':
+                action_options = ['DISMISS', 'MALICIOUS', 'TRUSTED']
+                action_option = input('Please select one of the following actions for Threat {}. {}\n'
+                                      .format(event['fileName'], action_options))
+                if action_option not in action_options:
+                    self.logger.error('No correct option selected. Pass.')
+                    break
+
+                self.take_action(action_option, md5, sha1, sha256, event['fileName'])
+
+            #sys.exit()
 
 
 if __name__ == '__main__':
@@ -177,6 +223,10 @@ if __name__ == '__main__':
 
     parser.add_argument('--hours', '-H', required=True,
                         type=int, help='Time to go back in hours')
+
+    parser.add_argument('--action', '-A', required=False,
+                        type=str, choices=['YES', 'NO'],
+                        default='NO', help='Should an action be applied to each collected Threat?')
 
     parser.add_argument('--loglevel', '-L', type=str,
                         required=False, default='INFO',
